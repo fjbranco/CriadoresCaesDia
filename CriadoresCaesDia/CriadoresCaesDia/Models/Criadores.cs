a649b9312cdbd7778c166f1e9382921ec06cdf10
fjbranco <estt6967@ipt.pt>
2021-03-22 16:47:21 +0000

    Continuação de construção inicial das classes Criadores e Fotografias

--- CriadoresCaesDia/CriadoresCaesDia/Models/Criadores.cs
+++ CriadoresCaesDia/CriadoresCaesDia/Models/Criadores.cs
@@ -5,7 +5,44 @@ using System.Threading.Tasks;
 
 namespace CriadoresCaesDia.Models
 {
+    /// <summary>
+    /// Descrição dos Criadores
+    /// </summary>
     public class Criadores
     {
+        /// <summary>
+        /// Identificador de cada Criador
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nome do criador
+        /// </summary>
+        
+        public string Nome { get; set; }
+        /// <summary>
+        /// Nome Comercial do criador
+        /// </summary>
+        
+        public string NomeComercial { get; set; }
+        /// <summary>
+        /// Morada do Criador
+        /// </summary>
+        
+        public string morada { get; set; }
+        /// <summary>
+        /// Código Postal da morada do criador
+        /// </summary>
+        
+        public string CodPostal { get; set; }
+        /// <summary>
+        /// número de telemóvel do criador
+        /// </summary>
+        public string Telemóvel { get; set; }
+
+        /// <summary>
+        /// Email do Criador
+        /// </summary>
+        public string Email { get; set; }
+
     }
 }
--- CriadoresCaesDia/CriadoresCaesDia/Models/Fotografias.cs
+++ CriadoresCaesDia/CriadoresCaesDia/Models/Fotografias.cs
@@ -5,7 +5,29 @@ using System.Threading.Tasks;
 
 namespace CriadoresCaesDia.Models
 {
+    /// <summary>
+    /// Fotografias referentes a cada cão
+    /// </summary>
     public class Fotografias
     {
+        /// <summary>
+        /// Identificador da fotografia
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Fotografia do Cão
+        /// </summary>
+        public int Fotografia { get; set; } // a ser corrigido o tipo de atributo
+
+        /// <summary>
+        /// Data da fotografia
+        /// </summary>
+        public DateTime DataFoto { get; set; }
+
+        /// <summary>
+        /// Local onde foi tirada a  fotografia
+        /// </summary>
+        public string LocalFoto { get; set; }
     }
 }
