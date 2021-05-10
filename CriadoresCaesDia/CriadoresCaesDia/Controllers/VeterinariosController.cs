abdc9e1bed9edb363c2e00b9b5dbfefd6af1f6b6
Filipe Branco <estt6967@ipt.pt>
2021-05-10 15:53:54 +0100

    Correcção dos seguintes problemas:    - Formatação da variável auxiliar para Decimal;    - PK duplicados.

--- CriadoresCaesDia/CriadoresCaesDia/Controllers/VeterinariosController.cs
+++ CriadoresCaesDia/CriadoresCaesDia/Controllers/VeterinariosController.cs
@@ -54,15 +54,51 @@ namespace CriadoresCaesDia.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Honorarios")] Veterinarios veterinarios)
+        public async Task<IActionResult> Create([Bind("Id,Nome,HonorarioAux")] Veterinarios veterinario)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(veterinarios);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                // adicionar o valor do HonorarioAux aos dados a guardar na base de dados
+                // converter o valor HonorarioAux para um formato que seja compreendido pela função convert
+                veterinario.HonorarioAux = veterinario.HonorarioAux.Replace('.',',');
+                //converter e adicionar aos dados que serão guardados na BD.
+                veterinario.Honorarios = Convert.ToDecimal(veterinario.HonorarioAux);
+
+                // será que o ID que o utilizador forneceu existe?
+                // procurar na BD se o ID proposto já existe
+                // SELECT * 
+                // FROM Veterinario
+                // WHERE Id = a um valor fornecido pelo utilizador
+                // v=> v.Id == veterinario.Id
+                    var _auxVeterinario = await _context.Veterinarios
+                    .FirstOrDefaultAsync(v => v.Id == veterinario.Id);
+                // avaliar se posso inserir os dados
+                if (_auxVeterinario == null) {
+                    try {
+                        _context.Add(veterinario);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    } catch (Exception excecao) {
+                        // adicionar os dados do erro numa tabela na BD
+                        //   - nome da pessoa que está a usar a app
+                        //   - data + hora do erro
+                        //   - nome do controller
+                        //   - nome do método
+                        //   - guardar o conteúdo do execao.Message
+                        //   - e do excecao.StackTrace
+
+                        // escrever os mesmos dados num ficheiro no disco ssd do server
+                        throw;
+                    }
+                    
+                } else {
+                    // o ID já existe na BD
+                    // devolver o controlo à View
+                    ModelState.AddModelError("","O código do veterinário já existe, por favor insira outro.");
+                }
             }
-            return View(veterinarios);
+            // se chego aqui é porque ocorreu algum erro
+            return View(veterinario);
         }
 
         // GET: Veterinarios/Edit/5
